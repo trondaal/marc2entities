7adb20c1ea1f669f58c13f63054e0f6a83303c27
trondaal <trondaal@ntnu.no>
2024-10-23 16:25:20 -0400

    Cleaning up examples

---
+++ examples/aggregates_project/run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "Creating xslt conversion"
+java -cp "../jar/saxon/saxon9he.jar"  net.sf.saxon.Transform -xsl:"../xslt/make.xslt" -s:"rules/example-rules.xml" -o:"xslt/marc2rda.xslt" 
+
+
+echo "Running transformation of local files in directory"
+java -cp "../jar/saxon/saxon9he.jar" net.sf.saxon.Transform -xsl:"xslt/marc2rda.xslt" -s:"input/examples.xml" -o:"output/examples.rdf" merge=true rdf=true
+
