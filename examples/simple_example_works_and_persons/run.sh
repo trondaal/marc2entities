5a415900589cc6bd8f39655187d014044ad7791b
trondaal <trondaal@ntnu.no>
2024-10-23 21:13:31 -0400

    Moving saxon to top level in example directory

--- examples/simple_example_works_and_persons/run.sh
+++ examples/simple_example_works_and_persons/run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 echo "Creating xslt conversion"
-java -cp "SaxonHE12-5J/saxon-he-12.5.jar"  net.sf.saxon.Transform -xsl:"../../xslt/make.xslt" -s:"rules/conversionrules.xml" -o:"xslt/marc2rda.xslt" 
+java -cp "../SaxonHE12-5J/saxon-he-12.5.jar"  net.sf.saxon.Transform -xsl:"../../xslt/make.xslt" -s:"rules/conversionrules.xml" -o:"xslt/marc2rda.xslt" 
 
 echo "Running transformation of local files in directory"
-java -cp "SaxonHE12-5J/saxon-he-12.5.jar" net.sf.saxon.Transform -xsl:"xslt/marc2rda.xslt" -s:"input/example.xml" -o:"output/example.rdf"
+java -cp "../SaxonHE12-5J/saxon-he-12.5.jar" net.sf.saxon.Transform -xsl:"xslt/marc2rda.xslt" -s:"input/example.xml" -o:"output/example.rdf"
 
