786c1ea6877bfa1938d8dc0c2f86eaee7d30207d
trondaal <trondaal@ntnu.no>
2024-10-23 21:04:16 -0400

    Adding simple example

--- examples/simple_example_works_and_persons/run.sh
+++ examples/simple_example_works_and_persons/run.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 echo "Creating xslt conversion"
-java -cp "../jar/saxon/saxon9he.jar"  net.sf.saxon.Transform -xsl:"../xslt/make.xslt" -s:"rules/example-rules.xml" -o:"xslt/marc2rda.xslt" 
-
+java -cp "SaxonHE12-5J/saxon-he-12.5.jar"  net.sf.saxon.Transform -xsl:"../../xslt/make.xslt" -s:"rules/conversionrules.xml" -o:"xslt/marc2rda.xslt" 
 
 echo "Running transformation of local files in directory"
-java -cp "../jar/saxon/saxon9he.jar" net.sf.saxon.Transform -xsl:"xslt/marc2rda.xslt" -s:"input/examples.xml" -o:"output/examples.rdf" merge=true rdf=true
+java -cp "SaxonHE12-5J/saxon-he-12.5.jar" net.sf.saxon.Transform -xsl:"xslt/marc2rda.xslt" -s:"input/example.xml" -o:"output/example.rdf"
 
